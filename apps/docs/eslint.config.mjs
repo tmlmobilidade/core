640e52e0864812a6478a7e22908d2d051cc2c5da
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-03-08 00:47:55 +0000

    Init docs

---
+++ apps/docs/eslint.config.mjs
@@ -0,0 +1,12 @@
+/* * */
+
+import { next } from '@carrismetropolitana/eslint'
+
+/* * */
+
+export default [
+  ...next,
+  {
+    files: ['source.config.ts', 'src/**/*.ts', 'src/**/*.tsx'],
+  },
+]

d601fe29ef74d4930abda3cbcea3231a87688dab
Josue Monteiro <jusi.monteiro@protonmail.com>
2025-03-20 10:47:32 +0000

    Fix Build Types

--- packages/core/scripts/build-types.ts
+++ packages/core/scripts/build-types.ts
@@ -12,9 +12,9 @@ const newPackageJson = {
 	...originalPackageJson,
 	exports: {
 		'.': {
-			types: './dist/core-types.d.ts',
-			import: './dist/core-types.mjs',
-			require: './dist/core-types.js',
+			types: './dist/src/core-types.d.ts',
+			import: './dist/src/core-types.mjs',
+			require: './dist/src/core-types.js',
 		},
 	},
 	name: '@tmlmobilidade/core-types',

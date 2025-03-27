46d0f0ded80344fdf849904309fe3fdeda180abd
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-03-27 16:08:48 +0000

    Remove unused AppWrapper import from AppProvider component

--- packages/ui/src/components/AppProvider/index.tsx
+++ packages/ui/src/components/AppProvider/index.tsx
@@ -10,8 +10,6 @@ import { MapProvider } from '@vis.gl/react-maplibre';
 import { type PropsWithChildren } from 'react';
 import { SWRConfig, type SWRConfiguration } from 'swr';
 
-import { AppWrapper } from '../layout';
-
 /* * */
 
 export function AppProvider({ children }: PropsWithChildren) {

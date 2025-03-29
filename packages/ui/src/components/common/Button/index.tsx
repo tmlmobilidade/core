14ca558a8e0271057904ca75384b0e29678b141f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-03-29 11:18:11 +0000

    Wrap MantineButton in a div for improved layout consistency

--- packages/ui/src/components/common/Button/index.tsx
+++ packages/ui/src/components/common/Button/index.tsx
@@ -33,19 +33,21 @@ export function Button({ disabled, href, icon, label, loading, onClick, type = '
 	// B. Render components
 
 	return (
-		<MantineButton
-			component={href ? 'a' : 'button'}
-			disabled={disabled || loading}
-			href={href}
-			leftSection={icon && icon}
-			loading={loading}
-			onClick={onClick}
-			type={type}
-			variant={variant}
-			{...props}
-		>
-			{label}
-		</MantineButton>
+		<div>
+			<MantineButton
+				component={href ? 'a' : 'button'}
+				disabled={disabled || loading}
+				href={href}
+				leftSection={icon && icon}
+				loading={loading}
+				onClick={onClick}
+				type={type}
+				variant={variant}
+				{...props}
+			>
+				{label}
+			</MantineButton>
+		</div>
 	);
 
 	//

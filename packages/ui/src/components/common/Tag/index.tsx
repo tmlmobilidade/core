9dc6b624e8863c88bdb3596c5d14792271db73a9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-04-02 17:54:44 +0100

    Add onClick prop to Tag component and update styles for clickable behavior

--- packages/ui/src/components/common/Tag/index.tsx
+++ packages/ui/src/components/common/Tag/index.tsx
@@ -13,14 +13,15 @@ export interface TagProps {
 	filled?: boolean
 	icon?: React.ReactNode
 	label?: number | string
+	onClick?: () => void
 	variant?: 'danger' | 'muted' | 'primary' | 'secondary' | 'success' | 'warning'
 }
 
 /* * */
 
-export function Tag({ filled = false, icon, label, variant = 'primary' }: TagProps) {
+export function Tag({ filled = false, icon, label, onClick, variant = 'primary' }: TagProps) {
 	return (
-		<div className={styles.tag} data-filled={filled} data-variant={variant}>
+		<div className={styles.tag} data-clickable={!!onClick} data-filled={filled} data-variant={variant} onClick={onClick}>
 			{React.isValidElement<IconProps>(icon) && <span className={styles.icon}>{React.cloneElement(icon)}</span>}
 			{label !== null && label !== undefined && <span className={styles.label}>{label}</span>}
 		</div>

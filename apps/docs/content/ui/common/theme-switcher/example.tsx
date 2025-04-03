b3a0861a41523a2bfa2ef2258529c505757e1903
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-04-03 21:34:53 +0100

    Add ThemeSwitcher component documentation and example; update AWS SDK dependencies

---
+++ apps/docs/content/ui/common/theme-switcher/example.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ComponentWrapper, ThemeSwitcher } from '@tmlmobilidade/ui';
+
+export default function ThemeSwitcherExample() {
+	return (
+		<ComponentWrapper>
+			<ThemeSwitcher />
+		</ComponentWrapper>
+	);
+}

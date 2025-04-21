3cef91e3a0929fa2f0bd795c912223c6ddaf4983
jusimen <jusi.monteiro@protonmail.com>
2025-04-21 14:33:41 +0100

    Add temporary agencies list and export in the library module

---
+++ packages/lib/src/agencies.ts
@@ -0,0 +1,27 @@
+/**
+ * THIS IS A TEMPORARY LIST OF AGENCIES.
+ *
+ * IT WILL BE REPLACED BY THE NEW DATABASE IMPLEMENTATION.
+ */
+export const AVAILABLE_AGENCIES = [
+	{
+		_id: '644976034212abfd6e160d1a',
+		name: 'Viação Alvorada',
+	},
+	{
+		_id: '645d7ee04ef63aec14fbf1eb',
+		name: 'Rodoviária de Lisboa',
+	},
+	{
+		_id: '645d7f204ef63aec14fbf22a',
+		name: 'Alsa Todi',
+	},
+	{
+		_id: '645d7f114ef63aec14fbf217',
+		name: 'TST',
+	},
+	{
+		_id: '67daeede2be7609f6510128c',
+		name: 'Cascais Próxima',
+	},
+];

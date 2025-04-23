937f7cbbf9e62642b2ba118c1d0492fd824b0d10
jusimen <jusi.monteiro@protonmail.com>
2025-04-23 10:26:19 +0100

    Add http utility functions for data fetching and file uploads

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -4,6 +4,7 @@ export * from '@/convert-object.js';
 export * from '@/files.js';
 export * from '@/generate-random-string.js';
 export * from '@/generate-random-token.js';
+export * from '@/http.js';
 export * from '@/operational-date.js';
 export * from '@/permissions.js';
 export * from '@/singleton-proxy.js';

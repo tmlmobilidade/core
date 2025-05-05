24d198715449a3294d4e2034ff7b1f263777ae92
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-05 16:23:13 +0100

    Updated packages, including mantine v8.0.0

--- apps/docs/content/ui/common/date-picker/example.tsx
+++ apps/docs/content/ui/common/date-picker/example.tsx
@@ -4,13 +4,13 @@ import { ComponentWrapper, DatePicker, Label } from '@tmlmobilidade/ui';
 import { useState } from 'react';
 
 export default function DatePickerExample() {
-	const [value, setValue] = useState<Date | null>(null);
+	const [value, setValue] = useState<null | string>(null);
 
 	return (
 		<ComponentWrapper>
 			<div className="flex flex-col gap-2" style={{ width: '30%' }}>
 				<DatePicker onChange={setValue} value={value} fullWidth />
-				<Label>Value: {value?.toLocaleDateString()}</Label>
+				<Label>Value: {value}</Label>
 			</div>
 		</ComponentWrapper>
 	);

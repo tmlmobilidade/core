291e321cecd616bf03de8e04113151b83691f37e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-05-13 17:57:51 +0100

    Add new property

--- packages/types/src/apex-on-board-refund.ts
+++ packages/types/src/apex-on-board-refund.ts
@@ -10,6 +10,7 @@ export const ApexOnBoardRefundSchema = ApexTransactionSchema.extend({
 	card_physical_type: z.number(),
 	card_serial_number: z.string(),
 	invoice_number: z.string().optional(),
+	is_enriched: z.boolean(),
 	on_board_sale_transaction_id: z.string().optional(),
 	payment_method: z.number(),
 	price: z.number(),

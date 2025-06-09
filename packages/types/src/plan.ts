6d99c6e61f131797ebde791c1f1fcfb36b44cc01
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-09 17:30:40 +0100

    Updated Plan model

--- packages/types/src/plan.ts
+++ packages/types/src/plan.ts
@@ -2,9 +2,8 @@
 
 import { DocumentSchema } from '@/_common/document.js';
 import { type UnixTimestamp } from '@/_common/unix-timestamp.js';
+import { GtfsAgencySchema, GtfsFeedInfoSchema } from '@/gtfs.js';
 import { z } from 'zod';
-
-import { GtfsAgency, GtfsAgencySchema, GtfsFeedInfo, GtfsFeedInfoSchema } from './gtfs.js';
 
 /* * */
 
@@ -12,27 +11,27 @@ const FEEDER_STATUS = ['waiting', 'processing', 'success', 'error'] as const;
 export const FeederStatusSchema = z.enum(FEEDER_STATUS);
 
 export const PlanSchema = DocumentSchema.extend({
-	file_id: z.string(),
+	feeder_status: FeederStatusSchema,
 	gtfs_agency: GtfsAgencySchema,
 	gtfs_feed_info: GtfsFeedInfoSchema,
 	is_approved: z.boolean().default(false),
 	is_locked: z.boolean().default(false),
+	operation_file_id: z.string(),
 	validation_id: z.string(),
 }).strict();
 
 export const CreatePlanSchema = z.object({
 	validation_id: z.string(),
 });
+
 export const UpdatePlanSchema = PlanSchema.partial();
 
 /* * */
 
 export type FeederStatus = z.infer<typeof FeederStatusSchema>;
 
-export interface Plan extends Omit<z.infer<typeof PlanSchema>, 'created_at' | 'gtfs_agency' | 'gtfs_feed_info' | 'updated_at'> {
+export interface Plan extends Omit<z.infer<typeof PlanSchema>, 'created_at' | 'updated_at'> {
 	created_at: UnixTimestamp
-	gtfs_agency: GtfsAgency
-	gtfs_feed_info: GtfsFeedInfo
 	updated_at: UnixTimestamp
 }
 

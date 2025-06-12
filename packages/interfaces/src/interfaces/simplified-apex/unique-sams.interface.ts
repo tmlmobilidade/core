5fff50176239f85057a95931ad5f74933985b590
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-12 15:44:13 +0100

    Add schemas to Unique Sam

--- packages/interfaces/src/interfaces/simplified-apex/unique-sams.interface.ts
+++ packages/interfaces/src/interfaces/simplified-apex/unique-sams.interface.ts
@@ -1,14 +1,17 @@
 /* * */
 
 import { MongoCollectionClass } from '@/mongo-collection.js';
-import { type CreateUniqueSamDto, type UniqueSam, type UpdateUniqueSamDto } from '@tmlmobilidade/types';
+import { type CreateUniqueSamDto, type UniqueSam, UniqueSamSchema, type UpdateUniqueSamDto, UpdateUniqueSamSchema } from '@tmlmobilidade/types';
 import { AsyncSingletonProxy } from '@tmlmobilidade/utils';
 import { IndexDescription } from 'mongodb';
+import { z } from 'zod';
 
 /* * */
 
 class UniqueSamsClass extends MongoCollectionClass<UniqueSam, CreateUniqueSamDto, UpdateUniqueSamDto> {
 	private static _instance: UniqueSamsClass;
+	protected override createSchema: z.ZodSchema = UniqueSamSchema;
+	protected override updateSchema: z.ZodSchema = UpdateUniqueSamSchema;
 
 	private constructor() {
 		super();

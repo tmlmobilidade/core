ab12f0db45a30be962ad5dba95eb85b74cb32e8f
jusimen <jusi.monteiro@protonmail.com>
2025-06-12 17:34:21 +0100

    Implementation of new Files class to handle files, csvs and zips

--- packages/utils/src/filesystem/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '@/filesystem/download-file-to-disk.js';
-export * from '@/filesystem/get-file-extension.js';
-export * from '@/filesystem/get-mime-type.js';
-export * from '@/filesystem/set-directory-permissions.js';
-export * from '@/filesystem/unzip-file-to-disk.js';

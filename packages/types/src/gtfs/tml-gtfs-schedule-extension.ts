1edfb729366fba1c531680a7a58c39ff3b620db1
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-25 00:45:45 +0100

    Add Extended Route

--- packages/types/src/gtfs/tml-gtfs-schedule-extension.ts
+++ packages/types/src/gtfs/tml-gtfs-schedule-extension.ts
@@ -1,9 +1,17 @@
 /* * */
 
-import { type Trip } from 'gtfs-types';
+import { type Route, type Trip } from 'gtfs-types';
 
 /* * */
 
 export interface Trip_TMLExtended extends Trip {
 	pattern_id: string
+}
+
+/* * */
+
+export interface Route_TMLExtended extends Route {
+	line_id: string
+	line_long_name: string
+	line_short_name: string
 }

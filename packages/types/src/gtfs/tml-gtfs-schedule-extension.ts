545cb65b84c6bbdf20071a59663e90f66d6d8d73
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-25 15:36:19 +0100

    Add extended Stop attributes

--- packages/types/src/gtfs/tml-gtfs-schedule-extension.ts
+++ packages/types/src/gtfs/tml-gtfs-schedule-extension.ts
@@ -1,6 +1,6 @@
 /* * */
 
-import { type Route, type Trip } from 'gtfs-types';
+import { type Route, type Stop, type Trip } from 'gtfs-types';
 
 /* * */
 
@@ -15,4 +15,12 @@ export interface Route_TMLExtended extends Route {
 	line_long_name: string
 	line_short_name: string
 	path_type: number
+}
+
+/* * */
+
+export interface Stop_TMLExtended extends Stop {
+	municipality_id: string
+	parish_id: string
+	region_id: string
 }

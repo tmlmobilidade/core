748b362a775770641a5d84f8acdf3054d7523ed3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-26 21:06:26 +0100

    Export geo utils

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -4,6 +4,7 @@ export * from '@/dates/index.js';
 export * from '@/files/files.js';
 export * from '@/generate-random-string.js';
 export * from '@/generate-random-token.js';
+export * from '@/geo/index.js';
 export * from '@/http.js';
 export * from '@/permissions.js';
 export * from '@/singleton-proxy.js';

82de1defd6058bfb15db71baa0969697fc617629
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-28 17:42:49 +0100

    Export function

--- packages/utils/src/geo/index.ts
+++ packages/utils/src/geo/index.ts
@@ -3,4 +3,5 @@ export * from '@/geo/constants.js';
 export * from '@/geo/conversions.js';
 export * from '@/geo/cut-line-at-length.js';
 export * from '@/geo/get-geofence-on-point.js';
+export * from '@/geo/is-point-in-polygon.js';
 export * from '@/geo/measurements.js';

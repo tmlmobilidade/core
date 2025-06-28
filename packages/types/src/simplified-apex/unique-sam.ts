434acafd907a9ba79991da115a266f5c8b6c522e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-28 04:33:50 +0100

    Updated UniqueSam type

--- packages/types/src/simplified-apex/unique-sam.ts
+++ packages/types/src/simplified-apex/unique-sam.ts
@@ -1,6 +1,7 @@
 /* * */
 
 import { DocumentSchema } from '@/_common/document.js';
+import { ProcessingStatus } from '@/_common/system.js';
 import { type UnixTimestamp, validateUnixTimestamp } from '@/_common/unix-timestamp.js';
 import { z } from 'zod';
 
@@ -10,11 +11,12 @@ export const UniqueSamSchema = DocumentSchema.extend({
 	_id: z.number(),
 	agency_id: z.string(),
 	device_id: z.string(),
+	is_complete: z.boolean().default(false),
 	latest_apex_version: z.string().nullable(),
+	message: z.string(),
 	seen_first_at: z.number().transform(validateUnixTimestamp).brand('UnixTimestamp').nullable(),
 	seen_last_at: z.number().transform(validateUnixTimestamp).brand('UnixTimestamp').nullable(),
-	status: z.enum(['missing_transactions', 'complete', 'error', 'pending']).default('pending'),
-	status_message: z.string().nullable(),
+	system_status: z.nativeEnum(ProcessingStatus).default(ProcessingStatus.Waiting),
 	transactions_expected: z.number().nullable(),
 	transactions_found: z.number().nullable(),
 	transactions_missing: z.number().nullable(),

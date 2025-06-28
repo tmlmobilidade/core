1463647b0b01308837460054eb80bd9fd24183a9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-28 17:37:06 +0100

    Add geo function

--- packages/utils/src/geo/index.ts
+++ packages/utils/src/geo/index.ts
@@ -2,4 +2,5 @@ export * from '@/geo/chunk-line.js';
 export * from '@/geo/constants.js';
 export * from '@/geo/conversions.js';
 export * from '@/geo/cut-line-at-length.js';
+export * from '@/geo/get-geofence-on-point.js';
 export * from '@/geo/measurements.js';

31456dfb2a5c83de9ffac4f45bb5b68f8b7cb1a0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-29 15:53:34 +0100

    Remove unnecessary property

--- packages/types/src/simplified-apex/unique-sam.ts
+++ packages/types/src/simplified-apex/unique-sam.ts
@@ -11,7 +11,6 @@ export const UniqueSamSchema = DocumentSchema.extend({
 	_id: z.number(),
 	agency_id: z.string(),
 	device_ids: z.array(z.string()).nullable(),
-	is_complete: z.boolean().nullable(),
 	latest_apex_version: z.string().nullable(),
 	remarks: z.string().nullable(),
 	seen_first_at: z.number().transform(validateUnixTimestamp).brand('UnixTimestamp').nullable(),

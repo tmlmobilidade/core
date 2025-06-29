32888fe21a37761c85bc6f58a78a42e38e9df175
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-29 14:12:54 +0100

    Cleanup

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,7 +2,6 @@ export * from '@/components/index';
 export * from '@/contexts/index';
 export * from '@/hooks/index';
 export * from '@/lib/map.utils';
-
 export * from '@mantine/form';
 export * from '@mantine/hooks';
 export * from '@mantine/modals';

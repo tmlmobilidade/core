7ee00b44d79f2da01b51c688578e196024b4f0fa
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-06-30 23:40:38 +0100

    Cleanup Fastify service

--- packages/connectors/src/fastify.ts
+++ packages/connectors/src/fastify.ts
@@ -2,54 +2,90 @@
 
 import cookie from '@fastify/cookie';
 import cors from '@fastify/cors';
-import fastify, { FastifyInstance, FastifyListenOptions, FastifyServerOptions } from 'fastify';
+import fastify, { type FastifyInstance, type FastifyServerOptions } from 'fastify';
 
 /* * */
 
-export interface ExtendedFastifyServiceOptions extends FastifyServerOptions {
+/**
+ * FastifyServiceOptions interface defines the options for the Fastify server.
+ * It extends FastifyServerOptions and adds optional properties for origin and port.
+ */
+export interface FastifyServiceOptions extends FastifyServerOptions {
+
+	/**
+	 * The origin for CORS requests.
+	 * Defaults to '*' if not provided.
+	 * @default '*'
+	 * @example 'https://example.com'
+	 */
 	origin?: string
+
+	/**
+	 * The port on which the Fastify server will listen.
+	 * If not provided, it defaults to 5050.
+	 * @default 5050
+	 */
+	port?: number
+
 }
 
 /* * */
 
 export class FastifyService {
+	//
+
 	private static _instance: FastifyService;
+
 	public readonly server: FastifyInstance;
+
 	private readonly origin: string;
 
-	private constructor(options: ExtendedFastifyServiceOptions) {
+	private readonly port: number;
+
+	/**
+	 * Creates an instance of FastifyService.
+	 * @param options The options for the Fastify server.
+	 */
+	private constructor(options: FastifyServiceOptions) {
 		this.server = fastify(options);
 		this.origin = options.origin || '*';
+		this.port = options.port || 5050;
 		this._setupDefaultRoutes();
 		this._setupPlugins();
 	}
 
-	public static getInstance(options?: FastifyServerOptions) {
+	/**
+	 * Gets the singleton instance of FastifyService.
+	 * @param options The options for the Fastify server.
+	 * @return The singleton instance of FastifyService.
+	 */
+	public static getInstance(options?: FastifyServiceOptions) {
 		if (!FastifyService._instance) {
+			// Create a new instance if it doesn't exist yet
 			FastifyService._instance = new FastifyService(options || {});
 		}
-
+		// Return the existing instance
 		return FastifyService._instance;
 	}
 
+	/**
+	 * Starts the Fastify server.
+	 * @return A promise that resolves when the server is started.
+	 */
 	async start() {
-		const options: FastifyListenOptions = {
-			host: process.env.HOST || '0.0.0.0',
-			port: Number(process.env.API_PORT) || 5050,
-		};
-
 		try {
-			await this.server.listen(options);
+			await this.server.listen({ port: this.port });
 		}
 		catch (error) {
-			this.server.log.error({
-				error,
-				message: 'Error starting server',
-			});
+			this.server.log.error({ error, message: 'Error starting server.' });
 			process.exit(1);
 		}
 	}
 
+	/**
+	 * Stops the Fastify server.
+	 * @return A promise that resolves when the server is stopped.
+	 */
 	async stop() {
 		try {
 			await this.server.close();
@@ -60,17 +96,24 @@ export class FastifyService {
 		}
 	}
 
+	/**
+	 * Sets the URL of the Fastify server.
+	 * @return The URL of the Fastify server.
+	 */
 	private _setupDefaultRoutes() {
 		this.server.get('/', (req, res) => {
 			res.send('Jusi was here!');
 		});
 	}
 
+	/**
+	 * Sets up the plugins for the Fastify server.
+	 * @return A promise that resolves when the plugins are set up.
+	 */
 	private async _setupPlugins() {
-		await this.server.register(cors, {
-			credentials: true,
-			origin: this.origin,
-		});
+		await this.server.register(cors, { credentials: true, origin: this.origin });
 		await this.server.register(cookie);
 	}
+
+	//
 }

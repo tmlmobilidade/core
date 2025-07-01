050206dde42fc003ce78de4669a1866dab79cd77
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-01 11:06:14 +0100

    Export Fastify types

--- packages/connectors/src/fastify.ts
+++ packages/connectors/src/fastify.ts
@@ -4,6 +4,8 @@ import cookie from '@fastify/cookie';
 import cors from '@fastify/cors';
 import fastify, { type FastifyInstance, type FastifyServerOptions } from 'fastify';
 
+export { type FastifyReply, type FastifyRequest } from 'fastify';
+
 /**
  * FastifyServiceOptions interface defines the options for the Fastify server.
  * It extends FastifyServerOptions and adds optional properties for origin and port.

d88b01543c7960616ca03cf7ea2b0c5499dce56d
jusimen <jusi.monteiro@protonmail.com>
2025-07-08 15:35:08 +0100

    Add welcome email functionality to email templates package

--- packages/email-templates/index.ts
+++ packages/email-templates/index.ts
@@ -3,7 +3,8 @@
 import { emailProvider } from '@tmlmobilidade/interfaces';
 
 import { ResetPasswordEmailProps } from './src/emails/reset-email';
-import { RenderResetPasswordEmail } from './src/renderer';
+import { WelcomeEmailProps } from './src/emails/welcome';
+import { RenderResetPasswordEmail, RenderWelcomeEmail } from './src/renderer';
 
 /* * */
 
@@ -21,6 +22,15 @@ export async function sendResetPasswordEmail(props: SendEmailProps<ResetPassword
 	await emailProvider.send({
 		html: emailHtml,
 		subject: 'Redefinição da sua palavra-passe',
+		to: props.to,
+	});
+};
+
+export async function sendWelcomeEmail(props: SendEmailProps<WelcomeEmailProps>) {
+	const emailHtml = RenderWelcomeEmail(props.props);
+	await emailProvider.send({
+		html: emailHtml,
+		subject: 'Bem-vindo ao GO!',
 		to: props.to,
 	});
 };
--- packages/email-templates/src/renderer.tsx
+++ packages/email-templates/src/renderer.tsx
@@ -6,9 +6,14 @@ import React from 'react';
 /* * */
 
 import { ResetPasswordEmail, ResetPasswordEmailProps } from './emails/reset-email';
+import { WelcomeEmail, WelcomeEmailProps } from './emails/welcome';
 
 /* * */
 
 export const RenderResetPasswordEmail = (props: ResetPasswordEmailProps) => {
 	return render(<ResetPasswordEmail {...props} />);
+};
+
+export const RenderWelcomeEmail = (props: WelcomeEmailProps) => {
+	return render(<WelcomeEmail {...props} />);
 };

07d80ef1b6c63f752adf79c23f3eb024926c37f0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-09 22:35:28 +0100

    Add TagGroup component

--- packages/ui/src/components/display/index.ts
+++ packages/ui/src/components/display/index.ts
@@ -1,4 +1,5 @@
 export * from '@/components/display/Label';
 export * from '@/components/display/NoDataLabel';
 export * from '@/components/display/Tag';
+export * from '@/components/display/TagGroup';
 export * from '@/components/display/Text';

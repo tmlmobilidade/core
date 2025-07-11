b814efc30e2b6bb2864c55fc74446ce525249ca5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-11 14:28:26 +0100

    Fix

--- packages/utils/src/navigation/keep-url-params.ts
+++ packages/utils/src/navigation/keep-url-params.ts
@@ -7,6 +7,7 @@
  * @param searchParams The search parameters to append. You can get this with `window.location.search`.
  * @returns A new URL string that combines the destination and the search parameters.
  */
-export function keepUrlParams(destination: string, searchParams: string): string {
+export function keepUrlParams(destination: string, searchParams?: string): string {
+	if (!searchParams) return destination;
 	return `${destination}${searchParams}`;
 }

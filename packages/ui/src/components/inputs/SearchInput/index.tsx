462b866b6da54f999c63b52ef6bdbf50797e9a77
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-11 14:39:09 +0100

    Add SearchInput

---
+++ packages/ui/src/components/inputs/SearchInput/index.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+/* * */
+
+import { ActionIcon as MantineActionIcon, TextInput as MantineTextInput } from '@mantine/core';
+import { IconSearch, IconX } from '@tabler/icons-react';
+
+/* * */
+
+export interface SearchInputProps {
+	onChange: (value: string) => void
+	value?: null | string
+}
+
+/* * */
+
+export function SearchInput({ onChange, value }: SearchInputProps) {
+	//
+
+	//
+	// A.Handle actions
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (!onChange) return;
+		onChange(event.target.value);
+	};
+
+	const handleClear = () => {
+		if (!onChange) return;
+		onChange('');
+	};
+
+	//
+	// B. Render components
+
+	return (
+		<MantineTextInput
+			leftSection={<IconSearch size={20} />}
+			onChange={handleChange}
+			placeholder="Pesquisar..."
+			value={value ?? ''}
+			rightSection={(
+				<MantineActionIcon onClick={handleClear} variant="subtle">
+					<IconX size={20} />
+				</MantineActionIcon>
+			)}
+		/>
+	);
+
+	//
+}
--- packages/ui/src/components/inputs/index.ts
+++ packages/ui/src/components/inputs/index.ts
@@ -1,3 +1,4 @@
 export * from '@/components/inputs/PasswordInput';
+export * from '@/components/inputs/SearchInput';
 export * from '@/components/inputs/Textarea';
 export * from '@/components/inputs/TextInput';

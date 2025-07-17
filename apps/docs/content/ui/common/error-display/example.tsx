3431e340c1a683f30f45b0d661a0772f72fe211e
Nitpreet <nitpreet.nitpreet@tmlmobilidade.pt>
2025-07-17 16:02:03 +0100

    Error Display added

---
+++ apps/docs/content/ui/common/error-display/example.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ComponentWrapper, ErrorDisplay } from '@tmlmobilidade/ui';
+
+export default function ErrorDisplayExample() {
+	return (
+		<ComponentWrapper>
+			<ErrorDisplay />
+		</ComponentWrapper>
+	);
+}

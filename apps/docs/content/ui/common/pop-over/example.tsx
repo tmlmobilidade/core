595b4ae79e2523747ffe79294c429329343d1e63
Nitpreet <nitpreet.nitpreet@tmlmobilidade.pt>
2025-07-17 15:07:14 +0100

    Popover added

--- apps/docs/content/ui/common/pop-over/example.tsx
+++ apps/docs/content/ui/common/pop-over/example.tsx
@@ -6,6 +6,7 @@ import {
 	PopoverProps as MantinePopoverProps,
 	PopoverTargetProps as MantinePopoverTargetProps,
 } from '@mantine/core';
+import { Button } from '@mantine/core';
 import { ComponentWrapper, Popover } from '@tmlmobilidade/ui';
 
 export type PopoverProps = MantinePopoverProps;
@@ -13,14 +14,16 @@ export type PopoverTargetProps = MantinePopoverTargetProps;
 export type PopoverDropdownProps = MantinePopoverDropdownProps;
 
 export default function PopoverExample() {
-	<ComponentWrapper>
-		<Popover
-		 Popover.Target = function PopoverTarget(props: PopoverTargetProps) {
-	return <MantinePopover.Target {...props} />;
-};
-
-Popover.Dropdown = function PopoverDropdown(props: PopoverDropdownProps) {
-	return <MantinePopover.Dropdown {...props} />;
-/>
-	</ComponentWrapper>;
+	return (
+		<ComponentWrapper>
+			<Popover>
+				<Popover.Target>
+					<Button>Open Popover</Button>
+				</Popover.Target>
+				<Popover.Dropdown>
+					<div>Add dropdown content</div>
+				</Popover.Dropdown>
+			</Popover>
+		</ComponentWrapper>
+	);
 }

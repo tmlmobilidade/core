afef3168b86b076cf6ea5b4106bd7a62a2ec8c55
Nitpreet <nitpreet.nitpreet@tmlmobilidade.pt>
2025-07-21 12:35:58 +0100

    Updated text-input

--- apps/docs/content/ui/common/text-input/example.tsx
+++ apps/docs/content/ui/common/text-input/example.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { IconSearch } from '@tabler/icons-react';
 import { ComponentWrapper, TextInput } from '@tmlmobilidade/ui';
 
 export default function TextInputExample() {
 	return (
 		<ComponentWrapper>
-			<TextInput leftSection={<IconSearch />} placeholder="Pesquisar..." tooltip="teste" />
+			<TextInput placeholder="Insert text" tooltip="Need help ?" />
 		</ComponentWrapper>
 	);
 }

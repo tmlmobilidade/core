1347db04c4872a6819e9e8bde2f6d7512f46b825
LoudNeo <meliodasgamer077@gmail.com>
2025-07-21 15:05:28 +0100

    Add hasAnySchema to stop.ts for improved status handling

--- packages/types/src/stop.ts
+++ packages/types/src/stop.ts
@@ -134,6 +134,12 @@ const FACILITIES_VALUES = [
 	'PIP',
 ] as const;
 
+const HAS_UNKNOWN = [
+	'YES',
+	'NO',
+	'UNKNOWN',
+] as const;
+
 //
 // Define schemas using constants
 
@@ -152,6 +158,7 @@ export const sidewalkTypeSchema = z.enum(SIDEWALK_TYPE_VALUES);
 export const connectionsSchema = z.array(z.enum(CONNECTIONS_VALUES));
 export const facilitiesSchema = z.array(z.enum(FACILITIES_VALUES));
 export const commentSchema = z.array(CommentSchema);
+export const hasAnySchema = z.enum(HAS_UNKNOWN);
 
 export const StopSchema = DocumentSchema.extend({
 

9154723152ed395c62a36139b3acd665179e1cd5
jusimen <jusi.monteiro@protonmail.com>
2025-07-21 16:55:43 +0100

    Add NumberInput component to inputs module and export it

--- packages/ui/src/components/inputs/index.ts
+++ packages/ui/src/components/inputs/index.ts
@@ -1,3 +1,4 @@
+export * from '@/components/inputs/NumberInput';
 export * from '@/components/inputs/PasswordInput';
 export * from '@/components/inputs/SearchInput';
 export * from '@/components/inputs/Textarea';

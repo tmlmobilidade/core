bd78548f3532b191a8559a435c7d135ae64edc83
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-25 02:42:43 +0100

    Add new column option to Grid

--- packages/ui/src/components/layout/index.ts
+++ packages/ui/src/components/layout/index.ts
@@ -1,7 +1,7 @@
-export { default as Grid } from './Grid';
 export * from '@/components/layout/AppWrapper';
 export * from '@/components/layout/Collapsible';
 export * from '@/components/layout/Divider';
+export * from '@/components/layout/Grid';
 export * from '@/components/layout/Section';
 export * from '@/components/layout/Spacer';
 export * from '@/components/layout/Surface';

1b38169206afffe5148db2612e7f25f1e0a291b1
jusimen <jusi.monteiro@protonmail.com>
2025-07-28 17:15:50 +0100

    Removed Mongodb from package "Types"

--- packages/types/src/_common/index.ts
+++ packages/types/src/_common/index.ts
@@ -2,7 +2,6 @@ export * from '@/_common/comment.js';
 export * from '@/_common/document.js';
 export * from '@/_common/email.js';
 export * from '@/_common/fastify.js';
-export * from '@/_common/mongo-operations.js';
 export * from '@/_common/operational-date.js';
 export * from '@/_common/proposed-change.js';
 export * from '@/_common/unix-timestamp.js';

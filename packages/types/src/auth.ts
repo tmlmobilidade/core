652247057646aa3196d356552965708f5f06e1a6
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-30 00:50:58 +0100

    Use snake case for properties

--- packages/types/src/auth.ts
+++ packages/types/src/auth.ts
@@ -31,7 +31,7 @@ export const UserSchema = DocumentSchema.extend({
 	phone: z.string().nullish(),
 	role_ids: z.array(z.string()).default([]),
 	session_ids: z.array(z.string()).default([]),
-	themeId: z.string().nullish(),
+	theme_id: z.string().nullish(),
 	verification_token_ids: z.array(z.string()).default([]),
 }).strict();
 

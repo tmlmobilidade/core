70c30346d526509b9762ac99d734c7140b9d5cfb
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-07-31 14:34:44 +0100

    More options on LockButton

--- packages/ui/src/components/buttons/LockButton/index.tsx
+++ packages/ui/src/components/buttons/LockButton/index.tsx
@@ -8,25 +8,51 @@ import { IconLockFilled, IconLockOpen2 } from '@tabler/icons-react';
 /* * */
 
 interface LockButtonProps {
+	isLoading?: boolean
 	isLocked: boolean
+	isReadOnly?: boolean
 	onClick: () => void
 }
 
 /* * */
 
-export function LockButton({ isLocked, onClick }: LockButtonProps) {
+export function LockButton({ isLoading, isLocked, isReadOnly, onClick }: LockButtonProps) {
 	//
+
+	//
+	// A. Handle actions
+
+	const handleClick = () => {
+		// If the button is loading or in read-only mode,
+		// do not trigger the onClick action
+		if (isLoading || isReadOnly) return;
+		// Trigger the onClick action
+		onClick();
+	};
+
+	//
+	// B. Render components
 
 	if (isLocked) {
 		return (
-			<ActionIcon color="var(--color-status-success-primary)" onClick={onClick} variant="subtle">
+			<ActionIcon
+				color="var(--color-status-success-primary)"
+				loading={isLoading}
+				onClick={handleClick}
+				variant="subtle"
+			>
 				<IconLockFilled />
 			</ActionIcon>
 		);
 	}
 
 	return (
-		<ActionIcon color="var(--color-primary)" onClick={onClick} variant="subtle">
+		<ActionIcon
+			color="var(--color-primary)"
+			loading={isLoading}
+			onClick={handleClick}
+			variant="subtle"
+		>
 			<IconLockOpen2 />
 		</ActionIcon>
 	);

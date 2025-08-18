a0d43cfaecc273012fcb1f8eb3fb8984406c56ee
jusimen <jusi.monteiro@protonmail.com>
2025-08-18 17:07:33 +0100

    Update @tmlmobilidade/emails dependency to wildcard version and add React imports in email components

--- packages/emails/src/components/highlight-text.tsx
+++ packages/emails/src/components/highlight-text.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@react-email/components';
+import React from 'react';
 
 import styles from './styles.js';
 
--- packages/emails/src/components/info-box.tsx
+++ packages/emails/src/components/info-box.tsx
@@ -1,4 +1,5 @@
 import { Section } from '@react-email/components';
+import React from 'react';
 
 import styles from './styles.js';
 

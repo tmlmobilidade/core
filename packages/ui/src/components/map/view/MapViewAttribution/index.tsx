05261872427a66b8df6dd7ca94fa0f940d96a0f0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-08-19 01:27:36 +0100

    CSS fix

--- packages/ui/src/components/map/view/MapViewAttribution/index.tsx
+++ packages/ui/src/components/map/view/MapViewAttribution/index.tsx
@@ -9,9 +9,9 @@ import styles from './styles.module.css';
 export function MapViewAttribution() {
 	return (
 		<div className={styles.attribution}>
-			<a href="https://maplibre.org/" target="_blank">MapLibre</a>
-			<a href="https://www.openmaptiles.org/" target="_blank">© OpenMapTiles</a>
-			<a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>
+			<a className={styles.link} href="https://maplibre.org/" target="_blank">MapLibre</a>
+			<a className={styles.link} href="https://www.openmaptiles.org/" target="_blank">© OpenMapTiles</a>
+			<a className={styles.link} href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>
 		</div>
 	);
 }

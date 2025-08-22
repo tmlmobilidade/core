00ad9c0b813ab91097d77795c5cb2d60c09c4684
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-08-22 15:50:48 +0100

    Fix state mismatch with autozoom toggle

--- packages/ui/src/components/map/view/MapViewContext/index.tsx
+++ packages/ui/src/components/map/view/MapViewContext/index.tsx
@@ -86,7 +86,6 @@ export const MapViewContextProvider = ({ children }: PropsWithChildren) => {
 	};
 
 	const centerMapOnRegisteredSources = () => {
-		console.log('centerMapOnRegisteredSources', 'mapRef.current', mapRef.current);
 		// Skip if the map is not available
 		if (!mapRef.current) return;
 		// Get the features to center the map on

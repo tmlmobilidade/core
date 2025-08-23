c0b8c51ae3f0d1fa622fd0c16c1828d782d862c7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-08-23 01:52:59 +0100

    Grow when used

--- packages/ui/src/components/inputs/SearchInput/index.tsx
+++ packages/ui/src/components/inputs/SearchInput/index.tsx
@@ -5,6 +5,8 @@
 import { ActionIcon as MantineActionIcon, TextInput as MantineTextInput } from '@mantine/core';
 import { IconSearch, IconX } from '@tabler/icons-react';
 
+import styles from './styles.module.css';
+
 /* * */
 
 export interface SearchInputProps {
@@ -18,7 +20,7 @@ export function SearchInput({ onChange, value }: SearchInputProps) {
 	//
 
 	//
-	// A.Handle actions
+	// A. Handle actions
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (!onChange) return;
@@ -35,6 +37,7 @@ export function SearchInput({ onChange, value }: SearchInputProps) {
 
 	return (
 		<MantineTextInput
+			classNames={{ root: styles.growWhenUsed, wrapper: styles.growWhenUsed }}
 			leftSection={<IconSearch size={20} />}
 			onChange={handleChange}
 			placeholder="Pesquisar..."

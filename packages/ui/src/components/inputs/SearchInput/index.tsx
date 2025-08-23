113bab68e3c030393c61877b916fa74fc354924b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-08-23 02:32:00 +0100

    Fix SearchInput style width fallback to 200

--- packages/ui/src/components/inputs/SearchInput/index.tsx
+++ packages/ui/src/components/inputs/SearchInput/index.tsx
@@ -56,7 +56,7 @@ export function SearchInput({ onChange, value }: SearchInputProps) {
 			onChange={handleChange}
 			onFocus={handleFocus}
 			placeholder="Pesquisar..."
-			styles={{ root: { width: isInUse ? '100%' : 'inherit' } }}
+			styles={{ root: { width: isInUse ? '100%' : 200 } }}
 			value={value ?? ''}
 			rightSection={
 				(typeof value === 'string' && value.length > 0) && (

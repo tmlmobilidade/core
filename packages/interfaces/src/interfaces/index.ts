8276b66e03a9d978c7e0b6dab17a4ff087cbea15
jusimen <jusi.monteiro@protonmail.com>
2025-08-25 17:41:29 +0100

    Export alerts_realtime interface in index.ts

--- packages/interfaces/src/interfaces/index.ts
+++ packages/interfaces/src/interfaces/index.ts
@@ -1,6 +1,7 @@
 /* * */
 
 export * from '@/interfaces/agencies/agencies.interface.js';
+export * from '@/interfaces/alerts/alerts-realtime.interface.js';
 export * from '@/interfaces/alerts/alerts.interface.js';
 export * from '@/interfaces/auth/roles.interface.js';
 export * from '@/interfaces/auth/sessions.interface.js';

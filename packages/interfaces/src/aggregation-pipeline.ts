ae2966857c8899aae796e193c48da65704060bc2
jusimen <jusi.monteiro@protonmail.com>
2025-08-27 18:06:06 +0100

    Enhance ProjectStage and SortStage interfaces to support additional string keys in aggregation pipeline

--- packages/interfaces/src/aggregation-pipeline.ts
+++ packages/interfaces/src/aggregation-pipeline.ts
@@ -2,14 +2,14 @@
 import { Filter } from 'mongodb';
 
 interface MatchStage<T> { $match: Filter<T> }
-interface ProjectStage<T> { $project: Partial<Record<keyof T, 0 | 1>> }
+interface ProjectStage<T> { $project: Partial<Record<keyof T, 0 | 1> | Record<string, 0 | 1>> }
 interface GroupStage {
 	$group: {
 		[key: string]: any
 		_id: Record<string, any> | string
 	}
 }
-interface SortStage<T> { $sort: Partial<Record<keyof T, -1 | 1>> }
+interface SortStage<T> { $sort: Partial<Record<keyof T, -1 | 1> | Record<string, -1 | 1>> }
 interface LimitStage { $limit: number }
 interface SkipStage { $skip: number }
 interface UnwindStage { $unwind: string | { path: string, preserveNullAndEmptyArrays: boolean } }

5f786d34e713d47ea7961e56a0b06e8c0ac28e1b
jusimen <jusi.monteiro@protonmail.com>
2025-08-28 15:53:26 +0100

    Add Separator component export to layout index file

--- packages/ui/src/components/layout/index.ts
+++ packages/ui/src/components/layout/index.ts
@@ -3,6 +3,7 @@ export * from '@/components/layout/Collapsible';
 export * from '@/components/layout/Divider';
 export * from '@/components/layout/Grid';
 export * from '@/components/layout/Section';
+export * from '@/components/layout/Separator';
 export * from '@/components/layout/Spacer';
 export * from '@/components/layout/Surface';
 export * from '@/components/layout/Toolbar';

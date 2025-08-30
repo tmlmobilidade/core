d8237c99a8ff1c32d904beb26fe02cc631044d97
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-08-30 02:11:08 +0100

    Add reason option

--- packages/types/src/rides/ride-analysis.ts
+++ packages/types/src/rides/ride-analysis.ts
@@ -29,7 +29,7 @@ export type AtLeastOneVehicleEventOnFirstStop = z.infer<typeof atLeastOneVehicle
 /* * */
 
 export const expectedDriverIdsQtySchema = RideAnalysisSchema.extend({
-	reason: z.enum(['NO_VEHICLE_EVENTS', 'UNEXPECTED_DRIVER_IDS_QTY', 'EXPECTED_DRIVER_IDS_QTY']),
+	reason: z.enum(['NO_PATH_DATA', 'NO_VEHICLE_EVENTS', 'UNEXPECTED_DRIVER_IDS_QTY', 'EXPECTED_DRIVER_IDS_QTY']),
 	value: z.number().nullable(),
 }).strict();
 

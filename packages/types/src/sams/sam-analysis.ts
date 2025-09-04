e42953068d37006ba133267276f25bf6c4abda6d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-04 10:40:30 +0100

    Add additional properties to schema

--- packages/types/src/sams/sam-analysis.ts
+++ packages/types/src/sams/sam-analysis.ts
@@ -1,6 +1,7 @@
 /* * */
 
 import { type UnixTimestamp, validateUnixTimestamp } from '@/_common/unix-timestamp.js';
+import { SimplifiedApexTypeSchema } from '@/simplified-apex/simplified-apex-type.js';
 import { z } from 'zod';
 
 /* * */
@@ -9,6 +10,10 @@ export const SamAnalysisSchema = z.object({
 	apex_version: z.string().nullable(),
 	device_id: z.string().nullable(),
 	end_time: z.number().transform(validateUnixTimestamp).brand('UnixTimestamp').nullable(),
+	first_transaction_id: z.string().nullable(),
+	first_transaction_type: SimplifiedApexTypeSchema.nullable(),
+	last_transaction_id: z.string().nullable(),
+	last_transaction_type: SimplifiedApexTypeSchema.nullable(),
 	start_time: z.number().transform(validateUnixTimestamp).brand('UnixTimestamp').nullable(),
 	transactions_expected: z.number().nullable(),
 	transactions_found: z.number().nullable(),
--- packages/types/src/simplified-apex/index.ts
+++ packages/types/src/simplified-apex/index.ts
@@ -3,4 +3,5 @@ export * from '@/simplified-apex/simplified-apex-inspection.js';
 export * from '@/simplified-apex/simplified-apex-location.js';
 export * from '@/simplified-apex/simplified-apex-on-board-refund.js';
 export * from '@/simplified-apex/simplified-apex-on-board-sale.js';
+export * from '@/simplified-apex/simplified-apex-type.js';
 export * from '@/simplified-apex/simplified-apex-validation.js';

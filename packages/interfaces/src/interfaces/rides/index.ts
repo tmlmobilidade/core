f1896dc4cf7f79e56b7cdbd66f936bfc133717d6
jusimen <jusi.monteiro@protonmail.com>
2025-09-11 12:52:56 +0100

    Remove RideAnnotationsClass and associated types from ride-annotations.ts, streamlining the codebase by eliminating unused components.

--- packages/interfaces/src/interfaces/rides/index.ts
+++ packages/interfaces/src/interfaces/rides/index.ts
@@ -1,4 +1,3 @@
 export * from '@/interfaces/rides/hashed-shapes.js';
 export * from '@/interfaces/rides/hashed-trips.js';
-export * from '@/interfaces/rides/ride-annotations.js';
 export * from '@/interfaces/rides/rides.js';

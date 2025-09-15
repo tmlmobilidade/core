e090e519a6281bdbf1c9a1cfc5213c9e5ed3fae0
jusimen <jusi.monteiro@protonmail.com>
2025-09-15 14:31:00 +0100

    Remove unused findById and findOne methods from OrganizationsClass to streamline code and improve maintainability.

--- packages/interfaces/src/interfaces/organizations/organizations.ts
+++ packages/interfaces/src/interfaces/organizations/organizations.ts
@@ -3,7 +3,7 @@
 import { MongoCollectionClass } from '@/mongo-collection.js';
 import { CreateOrganizationDto, CreateOrganizationSchema, Organization, OrganizationSchema, UpdateOrganizationDto, UpdateOrganizationSchema } from '@tmlmobilidade/types';
 import { AsyncSingletonProxy } from '@tmlmobilidade/utils';
-import { Filter, FindOptions, IndexDescription } from 'mongodb';
+import { IndexDescription } from 'mongodb';
 import { z } from 'zod';
 
 /* * */
@@ -24,30 +24,6 @@ class OrganizationsClass extends MongoCollectionClass<Organization, CreateOrgani
 			OrganizationsClass._instance = instance;
 		}
 		return OrganizationsClass._instance;
-	}
-
-	/**
-     * Finds a document by its ID.
-     *
-     * @param id - The ID of the document to find
-     * @returns A promise that resolves to the matching document or null if not found
-     */
-	override async findById(id: string, options?: FindOptions<Organization>) {
-		const organization = await this.mongoCollection.findOne({ _id: id } as unknown as Filter<Organization>, options);
-		if (!organization) {
-			return null;
-		}
-
-		return organization;
-	}
-
-	override async findOne(filter: Filter<Organization>) {
-		const organization = await this.mongoCollection.findOne(filter);
-		if (!organization) {
-			return null;
-		}
-
-		return organization;
 	}
 
 	protected getCollectionIndexes(): IndexDescription[] {

2dc7f4b2f0e8cf85c275f215cfa1b2a4f182ab53
jusimen <jusi.monteiro@protonmail.com>
2025-09-15 11:51:12 +0100

    Export ride justifications interface to enhance ride-related functionality and access control.

--- packages/interfaces/src/interfaces/rides/index.ts
+++ packages/interfaces/src/interfaces/rides/index.ts
@@ -1,3 +1,4 @@
 export * from '@/interfaces/rides/hashed-shapes.js';
 export * from '@/interfaces/rides/hashed-trips.js';
+export * from '@/interfaces/rides/ride-justifications.js';
 export * from '@/interfaces/rides/rides.js';

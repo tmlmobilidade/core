c850b364e2a72a4d791b2acf0e651b1c214a8584
jusimen <jusi.monteiro@protonmail.com>
2025-09-17 13:56:36 +0100

    Extend Comment schemas to include CRUD action types and enhance flexibility by adding CrudCommentSchema, improving comment handling capabilities.

--- packages/types/src/_common/comment.ts
+++ packages/types/src/_common/comment.ts
@@ -5,9 +5,13 @@ import { z } from 'zod';
 
 /* * */
 
-const COMMENT_TYPE_OPTIONS = ['field_changed', 'note'] as const;
+const COMMENT_TYPE_OPTIONS = ['field_changed', 'note', 'crud'] as const;
 export const CommentTypeSchema = z.enum(COMMENT_TYPE_OPTIONS);
 export type CommentType = z.infer<typeof CommentTypeSchema>;
+
+const CRUD_COMMENT_ACTION_OPTIONS = ['create', 'update', 'delete', 'archived', 'restored'] as const;
+export const CrudCommentSchemaActionSchema = z.enum(CRUD_COMMENT_ACTION_OPTIONS);
+export type CrudCommentSchemaAction = z.infer<typeof CrudCommentSchemaActionSchema>;
 
 /* * */
 
@@ -22,6 +26,11 @@ export const FieldChangedCommentSchema = DocumentSchema.extend({
 	metadata: z.record(z.unknown()).nullish(),
 	prev_value: z.any(),
 	type: z.literal(CommentTypeSchema.enum.field_changed),
+}).partial({ _id: true });
+
+export const CrudCommentSchema = DocumentSchema.extend({
+	action: CrudCommentSchemaActionSchema,
+	type: z.literal(CommentTypeSchema.enum.crud),
 }).partial({ _id: true });
 
 /* * */
@@ -42,6 +51,7 @@ export const CommentSchema = z
 	.discriminatedUnion('type', [
 		NoteCommentSchema,
 		FieldChangedCommentSchema,
+		CrudCommentSchema,
 	])
 	.superRefine((data, ctx) => {
 		if (data.type === CommentTypeSchema.enum.field_changed) {
@@ -53,6 +63,7 @@ export const CommentSchema = z
 
 export type Comment = z.infer<typeof CommentSchema>;
 export type NoteComment = z.infer<typeof NoteCommentSchema>;
+export type CrudComment = z.infer<typeof CrudCommentSchema>;
 export interface FieldChangedComment<T, K extends keyof T> extends Omit<z.infer<typeof FieldChangedCommentSchema>, 'curr_value' | 'field' | 'prev_value'> {
 	curr_value: T[K]
 	field: K

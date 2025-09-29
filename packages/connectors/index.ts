13e6778c51bdb2ede783ab06dbfa083e23812b99
jusimen <jusi.monteiro@protonmail.com>
2025-09-29 14:24:09 +0100

    feat: integrate Redis support by adding RedisService class and updating connectors

--- packages/connectors/index.ts
+++ packages/connectors/index.ts
@@ -5,6 +5,7 @@ export * from '@/fastify/index.js';
 export * from '@/mongo.js';
 export * from '@/postgres.js';
 export * from '@/rabbitmq.js';
+export * from '@/redis.js';
 export * from '@/sqlite/index.js';
 export * from '@/ssh-tunnel.js';
 export * from '@/trino.js';

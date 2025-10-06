93716abaad571682ac0d4237ccf8e9e8de3b6a6d
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-10-06 10:19:49 +0100

    refactor: update NotificationEmail component for improved text clarity and consistency

--- packages/emails/src/emails/notification-email.tsx
+++ packages/emails/src/emails/notification-email.tsx
@@ -14,36 +14,52 @@ export interface NotificationEmailProps {
 	topic: string
 }
 
+/* * */
+
 export function NotificationEmail({ body, href, priority, scope, title, topic }: NotificationEmailProps) {
+	//
+
+	//
+	// A. Setup variables
+	const priorityMap: Record<string, string> = {
+		high: 'Alta',
+		low: 'Baixa',
+		normal: 'Média',
+	};
+
+	//
+	// B. Render components
+
 	return (
-		<EmailWrapper preview="Pedido de aprovação de plano">
+		<EmailWrapper preview="Nova notificação">
 			<Section>
 				<Text style={styles.text}>
 					👋 Olá,
 				</Text>
 
 				<Text style={styles.text}>
-					Tens uma nova notificação
+					Tens uma nova notificação.
 				</Text>
 
 				<Hr style={{ margin: '24px 0' }} />
 
 				<InfoBox variant="info">
 					<Text style={{ ...styles.text, margin: '0 0 12px 0' }}>
-						<strong>📋 Detalhes da Notificação</strong>
+						<strong>🔔 Detalhes da Notificação</strong>
 					</Text>
 
 					<Text style={{ ...styles.text, margin: '8px 0' }}>
-						<strong>Titulo</strong>
+						<strong>Titulo:</strong>
 						{' '}
 						{title}
 						<br />
-						<strong>Descrição</strong>
+						<strong>Descrição:</strong>
+						{' '}
 						{body}
 						<br />
 						<strong>Prioridade:</strong>
 						{' '}
-						{priority}
+						{priorityMap[priority]}
 					</Text>
 				</InfoBox>
 
@@ -54,6 +70,8 @@ export function NotificationEmail({ body, href, priority, scope, title, topic }:
 			</Section>
 		</EmailWrapper>
 	);
+
+	//
 };
 
 export default NotificationEmail;

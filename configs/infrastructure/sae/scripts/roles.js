b0aa57337f0cc78aee01af44bbaa99ad7b5ed519
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-10-10 08:51:09 +0100

    Update roles.js

--- configs/infrastructure/sae/scripts/roles.js
+++ configs/infrastructure/sae/scripts/roles.js
@@ -165,6 +165,7 @@ db.createRole({
 		{ actions: ['find', 'listIndexes', 'collStats'], resource: { collection: 'simplified_apex_on_board_sales', db: 'production' } },
 		{ actions: ['find', 'listIndexes', 'collStats'], resource: { collection: 'simplified_apex_validations', db: 'production' } },
 		{ actions: ['find', 'listIndexes', 'collStats'], resource: { collection: 'ride_acceptances', db: 'production' } },
+		{ actions: ['find', 'listIndexes', 'collStats'], resource: { collection: 'metrics', db: 'production' } },
 	],
 	role: 'dgc-user',
 	roles: [],

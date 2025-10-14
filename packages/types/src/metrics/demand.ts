e94d9a9acc297640f436f7ebd92f4fa254dc16e5
Afonso Esteves <afonsomgesteves@gmail.com>
2025-10-14 15:25:48 +0100

    refactor: update demand schemas to include new metrics and improve organization

--- packages/types/src/metrics/demand.ts
+++ packages/types/src/metrics/demand.ts
@@ -4,14 +4,58 @@ import { z } from 'zod';
 
 import { MetricBasePropertiesSchema, MetricBaseSchema } from '@/metrics/common.js';
 
-// /metrics/demand_by_line_by_year?line=4701
-export const DemandByLineByYearSchema = MetricBaseSchema.extend({
+export const DemandByLineSchema = MetricBaseSchema.extend({
 	data: z.record(
-		z.string(), // e.g. "2024", "2025"
+		z.string(),
 		z.object({
 			qty: z.number(),
 		}),
 	),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+		line_id: z.string(),
+	}),
+});
+
+export const DemandByPatternSchema = MetricBaseSchema.extend({
+	data: z.record(
+		z.string(),
+		z.object({
+			qty: z.number(),
+		}),
+	),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+		pattern_id: z.string(),
+	}),
+});
+
+export const DemandRecordSchema = MetricBaseSchema.extend({
+	data: z.object({
+		operators: z.record(
+			z.string(),
+			z.record(
+				z.string(),
+				z.object({
+					qty: z.number(),
+				}),
+			),
+		),
+		total: z.record(
+			z.string(),
+			z.object({
+				qty: z.number(),
+			}),
+		),
+	}),
+	metric: z.literal('top_day_by_operator_and_total'),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+	}),
+});
+
+// /metrics/demand_by_line_by_year?line=4701
+export const DemandByLineByYearSchema = DemandByLineSchema.extend({
 	metric: z.literal('demand_by_line_by_year'),
 	properties: MetricBasePropertiesSchema.extend({
 		interval: z.literal(300000),
@@ -19,17 +63,63 @@ export const DemandByLineByYearSchema = MetricBaseSchema.extend({
 	}),
 });
 
-export const DemandByLineByMonthSchema = MetricBaseSchema.extend({
-	data: z.record(
-		z.string(), // e.g. "2024-01", "2024-02"
-		z.object({
-			qty: z.number(),
-		}),
-	),
+export const DemandByLineByMonthSchema = DemandByLineSchema.extend({
 	metric: z.literal('demand_by_line_by_month'),
 	properties: MetricBasePropertiesSchema.extend({
 		interval: z.literal(300000),
 		line_id: z.string(),
+	}),
+});
+
+export const DemandByLineByDaySchema = DemandByLineSchema.extend({
+	metric: z.literal('demand_by_line_by_day'),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+		line_id: z.string(),
+	}),
+});
+
+export const DemandByPatternByYearSchema = DemandByPatternSchema.extend({
+	metric: z.literal('demand_by_pattern_by_year'),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+		pattern_id: z.string(),
+	}),
+});
+
+export const DemandByPatternByMonthSchema = DemandByPatternSchema.extend({
+	metric: z.literal('demand_by_pattern_by_month'),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+		pattern_id: z.string(),
+	}),
+});
+
+export const DemandByPatternByDaySchema = DemandByPatternSchema.extend({
+	metric: z.literal('demand_by_pattern_by_day'),
+	properties: MetricBasePropertiesSchema.extend({
+		interval: z.literal(300000),
+		pattern_id: z.string(),
+	}),
+});
+
+export const DemandByPatternHourByYearSchema = DemandByPatternSchema.extend({
+	metric: z.literal('demand_by_pattern_hour_by_year'),
+	properties: MetricBasePropertiesSchema.extend({
+		hour: z.number().min(0).max(23),
+		interval: z.literal(300000),
+		minute: z.number().min(0).max(59),
+		pattern_id: z.string(),
+	}),
+});
+
+export const DemandByPatternHourByMonthSchema = DemandByPatternSchema.extend({
+	metric: z.literal('demand_by_pattern_hour_by_month'),
+	properties: MetricBasePropertiesSchema.extend({
+		hour: z.number().min(0).max(23),
+		interval: z.literal(300000),
+		minute: z.number().min(0).max(59),
+		pattern_id: z.string(),
 	}),
 });
 
--- packages/types/src/metrics/index.ts
+++ packages/types/src/metrics/index.ts
@@ -1,4 +1,4 @@
-import { DemandByLineByMonthSchema, DemandByLineByYearSchema } from '@/metrics/demand.js';
+import { DemandByLineByDaySchema, DemandByLineByMonthSchema, DemandByLineByYearSchema, DemandByPatternByDaySchema, DemandByPatternByMonthSchema, DemandByPatternByYearSchema, DemandByPatternHourByMonthSchema, DemandByPatternHourByYearSchema, DemandRecordSchema } from '@/metrics/demand.js';
 import { z } from 'zod';
 
 /* * */
@@ -6,6 +6,13 @@ import { z } from 'zod';
 export const MetricSchema = z.discriminatedUnion('metric', [
 	DemandByLineByYearSchema,
 	DemandByLineByMonthSchema,
+	DemandByLineByDaySchema,
+	DemandByPatternByYearSchema,
+	DemandByPatternByMonthSchema,
+	DemandByPatternByDaySchema,
+	DemandByPatternHourByYearSchema,
+	DemandByPatternHourByMonthSchema,
+	DemandRecordSchema,
 ]);
 
 /* * */

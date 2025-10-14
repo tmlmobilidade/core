68be829194dbe85cd61f5f296deae2a834628ac5
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-10-14 17:14:15 +0100

    Removed option to submit again in an existing proposed change

--- packages/ui/src/components/proposedChanges/ProposedChangesWrapperContentItemActions/index.tsx
+++ packages/ui/src/components/proposedChanges/ProposedChangesWrapperContentItemActions/index.tsx
@@ -30,7 +30,7 @@ export function ProposedChangesWrapperContentItemActions({ approve, isNew, permi
 	//
 	// B. Transform data
 
-	const visibleButtons = isNew ? buttons.filter(btn => btn.action === 'create') : buttons.filter(btn => permissions.find(p => p.action === btn.action));
+	const visibleButtons = isNew ? buttons.filter(btn => btn.action === 'create') : buttons.filter(btn => permissions.find(p => p.action === btn.action && btn.action !== 'create'));
 
 	//
 	// C. Render Components

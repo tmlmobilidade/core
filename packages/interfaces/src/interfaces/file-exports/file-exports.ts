ad676b9c3263dc8e6b7121cb30c4bf115a0b995f
Jusi Monteiro <jusi.monteiro@protonmail.com>
2025-10-20 16:21:11 +0100

    feat: implement FileExports class for file export management with MongoDB integration

---
+++ packages/interfaces/src/interfaces/file-exports/file-exports.ts
@@ -0,0 +1,55 @@
+/* * */
+
+import { MongoCollectionClass } from '@/mongo-collection.js';
+import { FileExportSchema } from '@tmlmobilidade/types';
+import { AsyncSingletonProxy } from '@tmlmobilidade/utils';
+import { IndexDescription } from 'mongodb';
+import { z } from 'zod';
+
+/* * */
+
+export type FileExportDocument = z.infer<typeof FileExportSchema>;
+
+/* * */
+
+class FileExportsClass extends MongoCollectionClass<FileExportDocument, FileExportDocument, FileExportDocument> {
+	private static _instance: FileExportsClass;
+	protected override createSchema: z.ZodSchema = FileExportSchema;
+	protected override updateSchema: z.ZodSchema = FileExportSchema;
+
+	private constructor() {
+		super();
+	}
+
+	public static async getInstance() {
+		if (!FileExportsClass._instance) {
+			const instance = new FileExportsClass();
+			await instance.connect();
+			FileExportsClass._instance = instance;
+		}
+		return FileExportsClass._instance;
+	}
+
+	protected getCollectionIndexes(): IndexDescription[] {
+		return [
+			{ background: true, key: { file_name: 1 } },
+			{ background: true, key: { processing_status: 1 } },
+			{ background: true, key: { scope: 1 } },
+			{ background: true, key: { type: 1 } },
+			{ background: true, expireAfterSeconds: 60_000 * 4 * 60, key: { created_at: 1 } }, // 4 hours
+			{ background: true, key: { updated_at: 1 } },
+		];
+	}
+
+	protected getCollectionName(): string {
+		return 'file_exports';
+	}
+
+	protected getEnvName(): string {
+		return 'DATABASE_URI';
+	}
+}
+
+/* * */
+
+export const fileExports = AsyncSingletonProxy(FileExportsClass);
